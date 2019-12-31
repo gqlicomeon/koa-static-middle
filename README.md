2a0de1f6398294c0fa16e1dac3575153f6c7470a
liguanqing <1244341574@qq.com>
2019-12-31 17:23:47 +0800

    koa-static-middle

---
+++ README.md
@@ -0,0 +1,43 @@
+# koa-static-middle
+A Static Middleware based on Koa
+## installation
+`$ npm install koa-static-middle`
+
+## Api
+```js
+    const koa = require("koa");
+    const app = new koa();
+    const static = require("koa-static-middle");
+    app.use(static(root,Opts));
+    app.listen(80);
+```
+* `root`root directory string. this is `neccessary`！！！
+* `opts`options Object
+
+### Opts
+* `maxAge`Browser cache max-age in milliseconds. defaults to 0
+* `index`Default file name, defaults to 'index.html'
+* `resHeader`Response header,defaults to '{}'
+
+## Example
+```js
+    const koa = require("koa");
+    const app = new koa();
+    const static = require("koa-static-middle");
+    //opts is {}
+    app.use(static("./static"));
+
+    //default file is index.txt
+    app.use(static("./static",{index:"index.txt"}));
+
+    //maxAge is 10
+    app.use(static("./static"),{maxAge:10});
+
+    //set resHeader,this is used by ctx.set()
+    app.use(static("./static",{resHeader:{"Cache-Control":"public","ETag":123}}));
+
+    //if opts.index is index.html, then http://127.0.0.1 will visit the path "./static/index.html"
+    app.listen(80);
+```
+## License
+`ISC`
---
+++ index.js
@@ -0,0 +1,116 @@
+/**
+ * Module dependencies.
+ */
+const mime = require("mime");
+const fs = require("fs");
+const path = require("path");
+/**
+ * staic()
+ */
+module.exports = static;
+
+/**
+ * 
+ * @param {String} [url] static root of server default:process.cwd()
+ * @param {Object} [opts] 
+ * @param {String} [opts.index] default "index.html"
+ * @param {Object} [opts.resHeader] response header
+ * 
+ */
+function static(url,opts={}){
+    opts = Object.assign({index:"index.html",maxAge:0,resHeader:{}},opts);
+    if(!path.isAbsolute(url)){
+        url = path.resolve("./",url);
+    }
+    return async function(ctx,next){
+        let reqPath;
+        if (ctx.method !== 'HEAD' && ctx.method !== 'GET'){
+            await next();
+            return;
+        };
+        if(ctx.path === "/"){
+            reqPath = path.resolve(url,opts.index);
+        }else{
+            reqPath = path.join(url,ctx.path);
+        }
+        try {
+            let stats = await getStats(reqPath);
+            if(stats.isFile()){
+                let type = mime.getType(reqPath);
+                let res;
+                if(/image|audio|video/.test(type)){
+                    res = await readBuffer(reqPath);
+                }else{
+                    res = await readFile(reqPath);
+                }
+                //set header
+                ctx.set("Cache-control",`max-age=${opts.maxAge}`);
+                ctx.set(opts.resHeader);
+                //set type
+                ctx.type = type; 
+                //set status
+                ctx.status = 200;
+                if (ctx.fresh) {
+                    //if fresh set Not Modified
+                    ctx.status = 304;
+                    return;
+                }
+                ctx.body = res;
+            }else{
+                await next();
+            }
+        } catch(e) {
+            await next();
+        }
+    }
+}
+/**
+ * 
+ * @param {String} url filepath
+ * @returns {Object} Promise thenable
+ */
+function readBuffer(url){
+    return new Promise((resolve,reject)=>{
+        let readStream = fs.createReadStream(url);
+        let data = [];
+        readStream.on("data",chunk=>{
+            data.push(chunk);
+        })
+        readStream.on("end",()=>{
+            resolve(Buffer.concat(data));
+        })
+        readStream.on("error",(err)=>{
+            reject(err);
+        })
+    })
+}
+/**
+ * 
+ * @param {String} url filepath
+ * @returns {Object} Promise thenable
+ */
+function readFile(url){
+    return new Promise((resolve,reject)=>{
+        fs.readFile(url,"utf8",(err,data)=>{
+            if(err){
+                reject(err);
+            } 
+            resolve(data);
+        })
+    })
+}
+/**
+ * 
+ * @param {String} url filepath
+ * @returns {Object} Promise thenable
+ */
+function getStats(url){
+    return new Promise((resolve,reject)=>{
+        fs.stat(url,(err,stats)=>{
+            if(err){
+                reject(err);
+            }
+            resolve(stats);
+        })
+    })
+}
